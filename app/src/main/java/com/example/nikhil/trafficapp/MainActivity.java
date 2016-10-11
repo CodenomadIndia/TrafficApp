432bfcf151eaad394246bce1232b7d45b1843cf4
nikhilcodenomad <nikhil@codenomad.net>
2016-10-11 18:47:16 +0530

    First Android Project

--- app/src/main/java/com/example/nikhil/trafficapp/MainActivity.java
+++ app/src/main/java/com/example/nikhil/trafficapp/MainActivity.java
@@ -17,8 +17,10 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -52,6 +54,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         validinput();
 
         if(isConnected()){
+            String url1 = "http://k53ldts.org/api/index.php/login/";
+            AsyncT ast = new AsyncT();
+            ast.execute(url1);
 
 
         }else{
@@ -77,13 +82,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             return false;
     }
 
-    class AsyncT extends AsyncTask<Void,Void,Void>{
+    class AsyncT extends AsyncTask<String,Void,Void>{
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Void doInBackground(String... params) {
 
             try {
-                URL url = new URL("http://codenomad.co/api/index.php"); //Enter URL here
+                URL url = new URL(params[0]); //Enter URL here
                 HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
                 httpURLConnection.setDoOutput(true);
                 httpURLConnection.setRequestMethod("POST"); // here you are telling that it is a POST request, which can be changed into "PUT", "GET", "DELETE" etc.
@@ -91,12 +96,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 httpURLConnection.connect();
 
                 JSONObject jsonObject = new JSONObject();
-                jsonObject.put("para_1", "arg_1");
+                jsonObject.put("username", "admin");
+                jsonObject.put("password", "admin");
 
                 DataOutputStream wr = new DataOutputStream(httpURLConnection.getOutputStream());
                 wr.writeBytes(jsonObject.toString());
                 wr.flush();
-                wr.close();
+               // wr.close();
+
+                StringBuilder sb = new StringBuilder();
+                int HttpResult = httpURLConnection.getResponseCode();
+                if (HttpResult == HttpURLConnection.HTTP_OK) {
+                    BufferedReader br = new BufferedReader(
+                            new InputStreamReader(httpURLConnection.getInputStream(), "utf-8"));
+                    String line = null;
+                    while ((line = br.readLine()) != null) {
+                        sb.append(line + "\n");
+                    }
+                    br.close();
+                    System.out.println("" + sb.toString());
+                } else {
+                    System.out.println(httpURLConnection.getResponseMessage());
+                }
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
